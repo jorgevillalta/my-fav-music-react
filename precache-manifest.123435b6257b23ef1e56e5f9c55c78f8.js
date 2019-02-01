edff0baf43a465658dc5784da457049f85dc29b7
Jorge Villalta <jvillalta@topcon.com>
2019-02-01 10:34:19 +0100

    Updates

---
+++ precache-manifest.123435b6257b23ef1e56e5f9c55c78f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fa93efd3ed6b8a5f75e39c0a524937f",
+    "url": "https://jorgevillalta.github.io/my-fav-music-react/static/media/unknown-album.6fa93efd.jpg"
+  },
+  {
+    "revision": "4e285105706409d53ba1",
+    "url": "https://jorgevillalta.github.io/my-fav-music-react/static/js/runtime~main.4e285105.js"
+  },
+  {
+    "revision": "8ab1824116d94439c3fd",
+    "url": "https://jorgevillalta.github.io/my-fav-music-react/static/js/main.8ab18241.chunk.js"
+  },
+  {
+    "revision": "816eb30e68c1574de84a",
+    "url": "https://jorgevillalta.github.io/my-fav-music-react/static/js/1.816eb30e.chunk.js"
+  },
+  {
+    "revision": "dd6717a80f02ecb0aa2fe1be3487da9f",
+    "url": "https://jorgevillalta.github.io/my-fav-music-react/index.html"
+  }
+];
